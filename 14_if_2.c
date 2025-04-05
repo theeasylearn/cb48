87c1e011c6bff8582b7a24d051be80042d30c3a6
theeasylearn <theeasylearn@gmail.com>
2025-04-05 17:54:09 +0530

    decision making statement

---
+++ 14_if_2.c
@@ -0,0 +1,32 @@
+/* 
+    write a program to findout given number is even(baki) or odd (eki)
+    steps
+    -------------------------------
+    1) create integer variable number, reminder 
+    2) accept input in number variable 
+    3) get reminder by dividing number with 2 
+        reminder =  number%2 
+    4) if reminder is 1  or -1
+        then display message number is odd 
+    5) if reminder is 0 
+        then display message number is even 
+*/
+#include<stdio.h>
+void main()
+{
+    int number, reminder;
+    printf("enter number");
+    scanf("%d",&number); //number = 10
+    reminder = number%2; //reminder 0
+    printf("\n reminder = %d",reminder);
+    if(reminder==1 || reminder==-1) // < > <= >= != ==
+    {
+        printf("\n number is odd");
+    }
+    if(reminder==0 )
+    {
+        printf("\n number is even");
+    }
+
+    printf("\n good bye");
+}
---
+++ 15_if_3.c
@@ -0,0 +1,2 @@
+/* write a program to accept month from user and display month name. */
+#include<stdio.h>
